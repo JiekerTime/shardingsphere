82495adec13bacef0375a94c9c67ab053acbbfb7
JiekerTime <jieker_mail@163.com>
2021-03-31 18:24:25 +0800

    add fixs

--- shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-fixture/src/test/java/org/apache/shardingsphere/test/integration/junit/container/adapter/impl/ShardingSphereProxyContainer.java
+++ shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-fixture/src/test/java/org/apache/shardingsphere/test/integration/junit/container/adapter/impl/ShardingSphereProxyContainer.java
@@ -43,9 +43,13 @@ public final class ShardingSphereProxyContainer extends ShardingSphereAdapterCon
     private final AtomicReference<DataSource> dataSourceProvider = new AtomicReference<>();
     
     public ShardingSphereProxyContainer(final ParameterizedArray parameterizedArray) {
-        super("shardingsphere-proxy", "apache/shardingsphere-proxy-test", parameterizedArray);
+        this(null, parameterizedArray);
     }
-    
+
+    public ShardingSphereProxyContainer(final String dockerName, final ParameterizedArray parameterizedArray) {
+        super(Objects.isNull(dockerName) ? "ShardingSphere-Proxy" : dockerName, "apache/shardingsphere-proxy-test",true, parameterizedArray);
+    }
+
     /**
      * Mount path into container from classpath.
      *
